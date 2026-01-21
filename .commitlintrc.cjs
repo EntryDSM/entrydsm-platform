e16a34bfb0615d3b37af2b39fe0a8248c892f03a
kangeunchan <kangeunchan080310@gmail.com>
2026-01-21 16:55:37 +0900

    fix(ci): rename .gitub to .github for templates (#2)
    Signed-off-by: kangeunchan <kangeunchan080310@gmail.com>

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['bug', 'feature', 'enhancement', 'docs', 'chore', 'hotfix', 'test', 'refactor']
+      ['fix', 'feature', 'enhancement', 'docs', 'chore', 'hotfix', 'test', 'refactor']
     ],
 
     'scope-empty': [2, 'never'],
