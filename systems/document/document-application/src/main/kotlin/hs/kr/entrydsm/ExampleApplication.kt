69077f8ca403632b7d80146fb2dc6c864b094b3f
kangeunchan <kangeunchan080310@gmail.com>
2026-01-22 14:35:54 +0900

    refactor(packages): kotlin 서비스 모듈명을 adapter-in/out, application, domain, bootstrap으로 표준화 (#14)

---
+++ systems/document/document-application/src/main/kotlin/hs/kr/entrydsm/ExampleApplication.kt
@@ -0,0 +1,11 @@
+package hs.kr.entrydsm.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExampleApplication>(*args)
+}
---
+++ systems/identity/identity-adapter-out/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
@@ -0,0 +1,11 @@
+package hs.kr.entrydsm.example
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleApplicationTest {
+    @Test
+    fun contextLoads() {
+        assertEquals(4, 2 + 2)
+    }
+}
