d1fbc14ee23f0e436b9148b3fc8d592ff1d72a29
kangeunchan <kangeunchan080310@gmail.com>
2026-03-03 11:49:03 +0900

    refactor(schedule): isolate bootstrap runtime module and config #31

--- systems/schedule/schedule-bootstrap/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
+++ systems/schedule/schedule-bootstrap/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
@@ -1,11 +1,11 @@
-package hs.kr.entrydsm.example
+package hs.kr.entrydsm.schedule
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-class ExampleApplicationTest {
+class ScheduleBootstrapApplicationTest {
     @Test
     fun contextLoads() {
-        assertEquals(4, 2 + 2)
+        assertTrue(true)
     }
 }
