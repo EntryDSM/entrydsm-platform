5d5390c594c64604dc805bd4ae6393e2b918e699
kangeunchan <kangeunchan080310@gmail.com>
2026-03-03 11:49:03 +0900

    refactor(gateway): isolate bootstrap runtime module and config #31

--- systems/gateway/gateway-adapter-out/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
+++ systems/gateway/gateway-adapter-out/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
@@ -1,11 +1,11 @@
-package hs.kr.entrydsm.example
+package hs.kr.entrydsm.gateway.adapterout
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-class ExampleApplicationTest {
+class GatewayAdapterOutModuleTest {
     @Test
-    fun contextLoads() {
-        assertEquals(4, 2 + 2)
+    fun moduleLoads() {
+        assertTrue(true)
     }
 }
