7f45cdd630050421f6ec307c4095075d3e1407d3
kangeunchan <kangeunchan080310@gmail.com>
2026-03-03 11:49:03 +0900

    refactor(document): isolate bootstrap runtime module and config #31

--- systems/document/document-application/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
+++ systems/document/document-application/src/test/kotlin/hs/kr/entrydsm/TestMain.kt
@@ -1,11 +1,11 @@
-package hs.kr.entrydsm.example
+package hs.kr.entrydsm.document.application
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-class ExampleApplicationTest {
+class DocumentApplicationModuleTest {
     @Test
-    fun contextLoads() {
-        assertEquals(4, 2 + 2)
+    fun moduleLoads() {
+        assertTrue(true)
     }
 }
